3c50cbebbd0bd4b7baf1098409e944ab81fd71e5
Vadiwoo <karuppiv@oregonstate.edu>
2018-02-15 11:15:48 -0800

    sign-out to destroy user-session

--- routes/sign-out.js
+++ routes/sign-out.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+var expressSession = require('express-session');
 
 router.get('/', (req, res) => {
-    req.logout();
-    res.redirect('/');
+    req.session.destroy(function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect('/');
+        }
+    });
 });
 
 module.exports = router;
